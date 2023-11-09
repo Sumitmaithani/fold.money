aec01f62401d1696b62442fe026a076e495d3846
Sumitmaithani <sumitmaithani824@gmail.com>
2023-11-09 22:14:03 +0530

    added more than row

--- src/components/landingPage/MoreThanRowComponent.js
+++ src/components/landingPage/MoreThanRowComponent.js
@@ -1,24 +1,34 @@
 "use-client";
 
 import Image from "next/image";
-import { motion, useAnimation } from "framer-motion";
+import { motion, useAnimation, useScroll, useTransform } from "framer-motion";
 import { useEffect } from "react";
 
 import icon_polygon from "@/assets/svgs/icon-polygon.svg";
+import left from "@/assets/images/left.png";
+import center from "@/assets/images/center.png";
+import right from "@/assets/images/right.png";
+import shop from "@/assets/svgs/shop.svg";
+import tyre from "@/assets/svgs/tyre.svg";
 
 export default function MoreThanRowComponent() {
-  const controls = useAnimation();
-
-  // Function to trigger animation on scroll
-  const handleScroll = () => {
-    const scrollY = window.scrollY;
-    controls.start({ x: scrollY });
+  const { scrollYProgress } = useScroll();
+  const options = {
+    // ease: [[0.7, 0, 0.84, 0], [0.7, 0, 0.84, 0], [0.7, 0, 0.84, 0]]
   };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  const x = useTransform(
+    scrollYProgress,
+    [0, 0.6, 1],
+    [0, -200, -800],
+    options
+  );
+  // const y = useTransform(scrollYProgress, [0, 0.7, 1], [-200, 0, 200], options);
+  const opacity = useTransform(
+    scrollYProgress,
+    [0.2, 0.3, 0.9, 1],
+    [0, 1, 1, 0],
+    options
+  );
 
   return (
     <div className="flow-root">
@@ -29,6 +39,42 @@ export default function MoreThanRowComponent() {
       <p className="mx-auto mt-4 text-soft w-fit">
         Not just the numbers, but the story as well
       </p>
+
+      <div className="flex gap-5 mt-40 mb-40">
+        <motion.div
+          style={{
+            x,
+            //y,
+            opacity,
+          }}
+          className="border-r border-softer"
+        >
+          <Image className="" src={right} alt="left" />
+          <div class="flex flex-col items-end gap-1 p-4 pt-0 pb-6 mt-6">
+            <Image src={shop} alt="shop" />
+            <p class="max-w-[25ch] text-right text-soft">
+              Merchants are detected automatically based on where you made your
+              purchase.
+            </p>
+          </div>
+        </motion.div>
+        {/* <motion.div>
+          <Image className="w-6/12" src={center} alt="left" />
+        </motion.div> */}
+        <motion.div className="border-l border-softer">
+          <Image className="" src={left} alt="right" />
+          <div class="p-4 pt-0 pb-6 mt-6 space-y-1 transition-opacity justify-self-end">
+            <Image src={tyre} alt="tyre" />
+            <p class="max-w-[30ch] text-left text-soft">
+              The{" "}
+              <a class="link" href="/blog/introducing-f1">
+                F1 engine
+              </a>{" "}
+              smartly categorises your transaction to an appropriate category.
+            </p>
+          </div>
+        </motion.div>
+      </div>
     </div>
   );
 }
